5118fc8e71fd53c227b6071d4d6bd91787370a86
berezzin <nexchanel@gmail.com>
2022-01-19 16:25:12 +0300

    Запилил MVP телеграм бота

---
+++ telegramBot.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot = telebot.TeleBot('5087141211:AAGOKkuVmk3bCOewKsNYjAnz3sf6FJiFT3g')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    print(message.text)
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
