69ff51529fd6e3e2de5b8f78573e069a6d92852c
berezzin <nexchanel@gmail.com>
2022-01-20 08:35:06 +0300

    Баг фикс - исправил баг с фамилией

--- telegramBot.py
+++ telegramBot.py
@@ -2,49 +2,42 @@ import telebot
 
 bot = telebot.TeleBot('5087141211:AAGOKkuVmk3bCOewKsNYjAnz3sf6FJiFT3g')
 
-
-# @bot.message_handler(content_types=['text'])
-# def get_text_messages(message):
-#     print(message.text)
-#     if message.text == "Привет":
-#         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-#     elif message.text == "/help":
-#         bot.send_message(message.from_user.id, "Напиши привет")
-#     else:
-#         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-
 name = ''
 surname = ''
 age = 0
+
+
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text == '/reg':
         bot.send_message(message.from_user.id, "Как тебя зовут?")
-        bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+        bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
     else:
         bot.send_message(message.from_user.id, 'Напиши /reg')
 
-def get_name(message): #получаем фамилию
+
+def get_name(message):  # получаем фамилию
     global name
     name = message.text
     bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
     bot.register_next_step_handler(message, get_surname)
 
+
 def get_surname(message):
     global surname
     surname = message.text
-    bot.send_message('Сколько тебе лет?')
+    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
     bot.register_next_step_handler(message, get_age)
+
 
 def get_age(message):
     global age
-    while age == 0: #проверяем что возраст изменился
+    while age == 0:  # проверяем что возраст изменился
         try:
-             age = int(message.text) #проверяем, что возраст введен корректно
+            age = int(message.text)  # проверяем, что возраст введен корректно
         except Exception:
-             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
-    bot.send_message(message.from_user.id, 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?')
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    bot.send_message(message.from_user.id, 'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?')
 
 
 bot.polling(none_stop=True, interval=0)
