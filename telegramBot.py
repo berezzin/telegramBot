b8d68e80d7720239c7319ae4e5420bff548ba1a6
berezzin <nexchanel@gmail.com>
2022-01-20 16:54:19 +0300

    Добавил вывод первых 10 товар в чат бота

--- telegramBot.py
+++ telegramBot.py
@@ -1,6 +1,6 @@
 import telebot
-from telebot import types
 from myParser import MyParser
+import json
 
 bot = telebot.TeleBot('5087141211:AAGOKkuVmk3bCOewKsNYjAnz3sf6FJiFT3g')
 
@@ -15,8 +15,19 @@ def start(message):
 
 
 def parsing(item_to_found):
+    n = 1
     MyParser.parse(item_to_found.text)
     bot.send_message(item_to_found.from_user.id, "Данные готовы в файле с форматом .json")
+    with open(f'jsonFiles/{item_to_found.text}_21vek.json', 'r', encoding='utf-8') as file:
+        data = json.loads(file.read())
+        for n, item in enumerate(data, start=n):
+            bot.send_message(item_to_found.from_user.id,
+                             f"№{str(n)}\n"
+                             f"Название товара: {item['itemName']}\n"
+                             f"Ссылка на товар: {item['itemLink']}\n"
+                             f"Цена: {item['itemPrice']}")
+            if n == 10:
+                break
 
 
 bot.polling(none_stop=True, interval=0)
